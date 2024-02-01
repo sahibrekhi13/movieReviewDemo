c08b430d9ec01d4f074eb719a238c9812a4ab852
Sahibjeet Rekhi <sahib@Sahibjeets-MacBook-Air.local>
2024-01-31 21:01:34 -0500

    pushing backen to the repo

---
+++ movies/src/main/java/com/demo/movies/ReviewService.java
@@ -0,0 +1,29 @@
+package com.demo.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Reviews CreateReview(String reviewBody, String imdbId) {
+
+        Reviews reviews = reviewRepository.insert(new Reviews(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(reviews))
+                .first();
+
+        return reviews;
+    }
+}
